0ce7e432c2620da205f609b09fd0e37397cb0583
TwistedFate <zhangpeng@zhilunkeji.com>
2020-09-11 17:37:21 +0800

    UIImage与UIButton类目添加

---
+++ TFKit-Objc/TFKit-Objc/Category/UIImage+TFKit.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+TFKit.h
+//  TFKit-Objc
+//
+//  Created by Twisted Fate on 2020/6/29.
+//  Copyright © 2020 Twisted Fate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (TFKit)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TFKit-Objc/TFKit-Objc/Category/UIImage+TFKit.m
@@ -0,0 +1,18 @@
+//
+//  UIImage+TFKit.m
+//  TFKit-Objc
+//
+//  Created by Twisted Fate on 2020/6/29.
+//  Copyright © 2020 Twisted Fate. All rights reserved.
+//
+
+#import "UIImage+TFKit.h"
+
+@implementation UIImage (TFKit)
+
+- (void)tf_imageWithColor:(UIColor *)color {
+    
+    
+}
+
+@end
